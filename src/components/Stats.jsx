c0c85d7e1b823258ca29246205390a7abd1ad2f6
JimmyCodeMunity <jameswafula2002@gmail.com>
2025-01-26 17:43:29 +0300

    mentheal ventures

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -24,7 +24,7 @@ const Stats = () => {
         <section className="py-14 bg-white">
             <div className="max-w-screen-xl mx-auto px-4 text-gray-600 gap-x-12 items-start justify-between lg:flex md:px-8">
                 <div className="sm:hidden lg:block lg:max-w-xl">
-                    <img src="https://images.unsplash.com/photo-1622675363311-3e1904dc1885?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" className="rounded-lg" alt="" />
+                    <img src="../images/thrapy1.jpg" className="rounded-lg" alt="" />
                 </div>
                 <div className="mt-6 gap-12 sm:mt-0 md:flex lg:block">
                     <div className="max-w-2xl">
