a84185bbf81627a80bfcbd81f6a09294ac488e40
JimmyCodeMunity <jameswafula2002@gmail.com>
2025-02-10 19:44:57 +0300

    updated

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -15,89 +15,96 @@ const Team = () => {
             </p>
           </div>
           <div className="space-y-5">
-          <div class="flex flex-wrap justify-center gap-y-14 max-w-3xl mx-auto lg:max-w-full ">
+            <div className="w-full text-center md:py-8 py-5">
+              <p className="text-3xl text-amber-500">Board of Advisors</p>
+            </div>
+            <div class="flex flex-wrap justify-center gap-y-14 max-w-3xl mx-auto lg:max-w-full ">
 
-{
-  chair.map((member) => {
-    return (
-      <div class="group block text-center lg:w-1/5 sm:w-1/3 min-[450px]:w-1/2 w-full">
-        <div class="relative mb-5">
-          <img
-            src={member.image}
-            alt="Guy image"
-            class="w-32 h-32 rounded-2xl object-cover mx-auto ransition-all duration-500 border-2 border-solid border-transparent group-hover:border-amber-400"
-          />
-        </div>
-        <h4 class="text-xl text-gray-900 font-semibold text-center mb-2 transition-all duration-500 group-hover:text-amber-400">
-          {member.name}
-        </h4>
-        <span class="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
-          {member.role}
-        </span>
-        <span class="text-gray-500 text-md text-center block transition-all duration-500 group-hover:text-gray-900">
-          {member.other}
-        </span>
-      </div>
-    )
-  })
-}
+              {
+                chair.map((member) => {
+                  return (
+                    <div class="group block text-center lg:w-1/5 sm:w-1/3 min-[450px]:w-1/2 w-full">
+                      <div class="relative mb-5">
+                        <img
+                          src={member.image}
+                          alt="Guy image"
+                          class="w-32 h-32 rounded-2xl object-cover mx-auto ransition-all duration-500 border-2 border-solid border-transparent group-hover:border-amber-400"
+                        />
+                      </div>
+                      <h4 class="text-xl text-gray-900 font-semibold text-center mb-2 transition-all duration-500 group-hover:text-amber-400">
+                        {member.name}
+                      </h4>
+                      <span class="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
+                        {member.role}
+                      </span>
+                      <span class="text-gray-500 text-md text-center block transition-all duration-500 group-hover:text-gray-900">
+                        {member.other}
+                      </span>
+                    </div>
+                  )
+                })
+              }
 
-</div><div class="flex flex-wrap justify-center gap-y-14 max-w-3xl mx-auto lg:max-w-full ">
+            </div><div class="flex flex-wrap justify-center gap-y-14 max-w-3xl mx-auto lg:max-w-full ">
 
-{
-  midlevel.map((member) => {
-    return (
-      <div class="group block text-center lg:w-1/5 sm:w-1/3 min-[450px]:w-1/2 w-full">
-        <div class="relative mb-5">
-          <img
-            src={member.image}
-            alt="Guy image"
-            class="w-32 h-32 rounded-2xl object-cover mx-auto ransition-all duration-500 border-2 border-solid border-transparent group-hover:border-amber-400"
-          />
-        </div>
-        <h4 class="text-xl text-gray-900 font-semibold text-center mb-2 transition-all duration-500 group-hover:text-amber-400">
-          {member.name}
-        </h4>
-        <span class="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
-          {member.role}
-        </span>
-        <span class="text-gray-500 text-md text-center block transition-all duration-500 group-hover:text-gray-900">
-          {member.other}
-        </span>
-      </div>
-    )
-  })
-}
+              {
+                midlevel.map((member) => {
+                  return (
+                    <div class="group block text-center lg:w-1/5 sm:w-1/3 min-[450px]:w-1/2 w-full">
+                      <div class="relative mb-5">
+                        <img
+                          src={member.image}
+                          alt="Guy image"
+                          class="w-32 h-32 rounded-2xl object-cover mx-auto ransition-all duration-500 border-2 border-solid border-transparent group-hover:border-amber-400"
+                        />
+                      </div>
+                      <h4 class="text-xl text-gray-900 font-semibold text-center mb-2 transition-all duration-500 group-hover:text-amber-400">
+                        {member.name}
+                      </h4>
+                      <span class="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
+                        {member.role}
+                      </span>
+                      <span class="text-gray-500 text-md text-center block transition-all duration-500 group-hover:text-gray-900">
+                        {member.other}
+                      </span>
+                    </div>
+                  )
+                })
+              }
 
-</div>
-<div class="flex flex-wrap justify-center gap-y-14 max-w-3xl mx-auto lg:max-w-full ">
+            </div>
 
-{
-  teammembers.map((member) => {
-    return (
-      <div class="group block text-center lg:w-1/5 sm:w-1/3 min-[450px]:w-1/2 w-full">
-        <div class="relative mb-5">
-          <img
-            src={member.image}
-            alt="Guy image"
-            class="w-32 h-32 rounded-2xl object-cover mx-auto ransition-all duration-500 border-2 border-solid border-transparent group-hover:border-amber-400"
-          />
-        </div>
-        <h4 class="text-xl text-gray-900 font-semibold text-center mb-2 transition-all duration-500 group-hover:text-amber-400">
-          {member.name}
-        </h4>
-        <span class="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
-          {member.role}
-        </span>
-        <span class="text-gray-500 text-md text-center block transition-all duration-500 group-hover:text-gray-900">
-          {member.other}
-        </span>
-      </div>
-    )
-  })
-}
+            <div className="w-full text-center md:py-8 py-5">
+              <p className="text-3xl text-amber-500">Operations Team</p>
+            </div>
+            <div class="flex flex-wrap justify-center gap-y-14 max-w-3xl mx-auto lg:max-w-full ">
 
-</div>
+              {
+                teammembers.map((member) => {
+                  return (
+                    <div class="group block text-center lg:w-1/5 sm:w-1/3 min-[450px]:w-1/2 w-full">
+                      <div class="relative mb-5">
+                        <img
+                          src={member.image}
+                          alt="Guy image"
+                          class="w-32 h-32 rounded-2xl object-cover mx-auto ransition-all duration-500 border-2 border-solid border-transparent group-hover:border-amber-400"
+                        />
+                      </div>
+                      <h4 class="text-xl text-gray-900 font-semibold text-center mb-2 transition-all duration-500 group-hover:text-amber-400">
+                        {member.name}
+                      </h4>
+                      <span class="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
+                        {member.role}
+                      </span>
+                      <span class="text-gray-500 text-md text-center block transition-all duration-500 group-hover:text-gray-900">
+                        {member.other}
+                      </span>
+                    </div>
+                  )
+                })
+              }
+
+            </div>
           </div>
         </div>
       </section>
